41e272034d3689c6ae4bb8a926d54605cdbf09d5
Aleksei K <awuxos@gmail.com>
2019-06-16 11:27:49 +0300

    NtpScore: Add explainable server scoring function

---
+++ src/core/ntp/ntp_score.c
@@ -0,0 +1,31 @@
+#include "ntp_score.h"
+
+#include <math.h>
+
+double gw_ntp_score(const gw_ntp_checker_result_t *result)
+{
+    double score = 1.0;
+
+    if (result == NULL) {
+        return 0.0;
+    }
+
+    score *= result->reachability;
+
+    if (result->delay_min_ms > 100.0) {
+        score -= 0.15;
+    }
+    if (result->jitter_ms > 10.0) {
+        score -= 0.20;
+    }
+    if (fabs(result->offset_median_ms) > 100.0) {
+        score -= 0.25;
+    }
+    if (score < 0.0) {
+        score = 0.0;
+    }
+    if (score > 1.0) {
+        score = 1.0;
+    }
+    return score;
+}
---
+++ src/core/ntp/ntp_score.h
@@ -0,0 +1,8 @@
+#ifndef GW32TIME_NTP_SCORE_H
+#define GW32TIME_NTP_SCORE_H
+
+#include "ntp_checker.h"
+
+double gw_ntp_score(const gw_ntp_checker_result_t *result);
+
+#endif
